8969e1fc2effa0b34bc785c495b74ee491233881
mac030 <mac030@mac030tekiiMac.local>
2017-10-20 11:10:53 +0800

    finish test case mtTest.js, not implemented yet

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # mt
-英翻中
+
+極小版的簡易英翻中系統
+
+## Example
+
+程式: dogMt.js 
+
+```js
+var mt = require('mini-mt')
+console.log(mt.e2c('A dog chase a cat')
+```
+
+執行結果
+
+```
+一隻 狗 追 一隻 貓
+```
+
+
+
+
+
+
---
+++ test/mtTest.js
@@ -0,0 +1,13 @@
+
+/* eslint-disable no-undef */
+var expect = require('chai').expect
+
+describe('mt', function () {
+    var mt = require('../lib/mt')
+    
+    describe('英翻中', function () {
+        it('mt.e2c(A dog chase a cat)', function () {
+            expect(mt.e2c('A dog chase a cat')).to.equal('一隻 狗 追 一隻 貓')
+        })  
+    })
+})
